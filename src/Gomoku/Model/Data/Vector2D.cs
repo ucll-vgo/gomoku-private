59b18ef2a58a12d8fc81d2ba4390326ef1edfe69
Frederic Vogels <frederic.vogels@ucll.be>
2020-11-08 16:11:14 +0100

    added comments to Vector2D

--- src/Gomoku/Model/Data/Vector2D.cs
+++ src/Gomoku/Model/Data/Vector2D.cs
@@ -5,16 +5,30 @@ using System.Text;
 
 namespace Model.Data
 {
+    /// <summary>
+    /// A Vector2D consists of two integers named X and Y.
+    /// </summary>
     public class Vector2D : IEquatable<Vector2D>
     {
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="x">X-coordinate</param>
+        /// <param name="y">Y-coordinate</param>
         public Vector2D(int x, int y)
         {
             this.X = x;
             this.Y = y;
         }
 
+        /// <summary>
+        /// X-coordinate of the Vector2D
+        /// </summary>
         public int X { get; }
 
+        /// <summary>
+        /// X-coordinate of the Vector2D
+        /// </summary>
         public int Y { get; }
 
         public override string ToString() => $"({X}, {Y})";
@@ -25,14 +39,43 @@ namespace Model.Data
 
         public override int GetHashCode() => X.GetHashCode() ^ Y.GetHashCode();
 
+        /// <summary>
+        /// Vector addition.
+        /// </summary>
+        /// <param name="u">First vector</param>
+        /// <param name="v">Second vector</param>
+        /// <returns>Sum of both vectors</returns>
         public static Vector2D operator +(Vector2D u, Vector2D v) => new Vector2D(u.X + v.X, u.Y + v.Y);
 
+        /// <summary>
+        /// Vector negation.
+        /// </summary>
+        /// <param name="v">Vector to be negated</param>
+        /// <returns>Negation of the vector</returns>
         public static Vector2D operator -(Vector2D v) => new Vector2D(-v.X, -v.Y);
 
+        /// <summary>
+        /// Vector subtraction.
+        /// </summary>
+        /// <param name="u">Vector to subtract from</param>
+        /// <param name="v">Vector to subtract</param>
+        /// <returns>Subtraction of v from u</returns>
         public static Vector2D operator -(Vector2D u, Vector2D v) => new Vector2D(u.X - v.X, u.Y - v.Y);
 
+        /// <summary>
+        /// Multiplication between vector and number
+        /// </summary>
+        /// <param name="v">Vector</param>
+        /// <param name="f">Factor</param>
+        /// <returns>Product</returns>
         public static Vector2D operator *(Vector2D v, int f) => new Vector2D(v.X * f, v.Y * f);
 
+        /// <summary>
+        /// Multiplication between vector and number
+        /// </summary>
+        /// <param name="v">Vector</param>
+        /// <param name="f">Factor</param>
+        /// <returns>Product</returns>
         public static Vector2D operator *(int f, Vector2D v) => v * f;
 
         public static readonly Vector2D NORTH = new Vector2D(0, 1);
@@ -51,6 +94,9 @@ namespace Model.Data
 
         public static readonly Vector2D SOUTHWEST = SOUTH + WEST;
 
+        /// <summary>
+        /// Returns a list of vectors pointing in all 8 directions.
+        /// </summary>
         public static IEnumerable<Vector2D> AllDirections
         {
             get

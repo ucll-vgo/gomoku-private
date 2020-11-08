45105cf6416a08ea9cfa659389f28b0d74823c6b
Frederic Vogels <frederic.vogels@ucll.be>
2020-11-08 16:12:52 +0100

    added comments to Stone

--- src/Gomoku/Model/Gomoku/Stone.cs
+++ src/Gomoku/Model/Gomoku/Stone.cs
@@ -4,12 +4,24 @@ using System.Text;
 
 namespace Model.Gomoku
 {
+    /// <summary>
+    /// Represents a stone. A stone can be either black or white.
+    /// </summary>
     public abstract class Stone
     {
+        /// <summary>
+        /// White stone.
+        /// </summary>
         public static readonly Stone WHITE = new WhiteStone();
 
+        /// <summary>
+        /// Black stone.
+        /// </summary>
         public static readonly Stone BLACK = new BlackStone();
         
+        /// <summary>
+        /// Opponent's stone. Black returns white and vice versa.
+        /// </summary>
         public abstract Stone Other { get; }
 
         public class WhiteStone : Stone
